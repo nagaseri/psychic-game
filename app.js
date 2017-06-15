0fa0deeca3c3a3993a86852681ebb23e90761a8f
Eri <eriinwanderland@Eris-MacBook-Pro.local>
2017-06-14 18:04:48 -0700

    updating the js

--- app.js
+++ app.js
@@ -1,23 +1,67 @@
-//create needed global variables 
+//create global variables 
 
 	var wins = 0;
 	var losses = 0;
-	var guessesLeft = 10;
-	var wrongGuesses = [ ];
-	var alphabet = [ 'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
+	var guessesLeft = 3;
+	var wrongGuesses = [];
+	var computerChoice = "";
+	var userChoice = "";
 
-//the computer picks a letter
+//the computer picks a random letter
+	function computerMakesChoice() {
+		var alphabet = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
+		var randomIndex = Math.floor(Math.random () * alphabet.length);
+		console.log('randomIndex', randomIndex);
+//the var computer choice is the alphabet randomly chosen
+		var computerChoice = alphabet[randomIndex];
+		console.log('computerChoice', computerChoice);
+	}
+//calling the function for computer to make choice
+	computerMakesChoice();
 	
-	var randomIndex = Math.floor(Math.random () * alphabet.length);
-	console.log('randomIndex', randomIndex);
-	
+//function to reset game	
+	function gameReset(){
+		guessesLeft = 3;
+		wrongGuesses = [];
+		computerMakesChoice();
+
+	}
 
 //the user picks a letter of the alphabet 
-//we need to compare the two 
+		document.onkeyup = function(event){
+			userChoice = event.key;
+			//if user guesses right letter, WIN
+			//increment wins counter
+			//reset guesses left, wrong guesses, and randomize computer choice
+			if(userChoice === computerChoice){
+				wins++;
+				gameReset();
+				console.log('wins', wins);
+			}
+			//if not, guesses left decreases
+			//push current user choice to array of wrongguesses
+			else {
+				guessesLeft--;
+				wrongGuesses.push(userChoice);
+				console.log('wrongGuesses', wrongGuesses);
+				console.log('userChoice', userChoice);
+				console.log('guessesLeft', guessesLeft);
+			}
+			//if there are no guesses left
+			//increment loss counter
+			//reset game
+			if(guessesLeft === 0){
+				losses++;
+				gameReset();
+				console.log('losses', losses);
+				console.log('guessesLeft', guessesLeft);
+			}
+			// if(userChoice === alphabet){
+			// 	userChoice.push(wrongGuesses);
+			// 	alert("You already guessed this letter, try again!")
+			// }
+			}
 
-//if they match, the user wins **START WITH EASIEST TASK FIRST**
-	//increment wins counter
-	//restart game
 
 //if they don't match
 	//decrement guess counter
@@ -26,7 +70,7 @@
 		//push the wrong letter guessed into array of wrong guesses
 	//if user has 0 guesses left
 		//user loses
-		//restart the game
+		//restart the game **RESET EVERTYTHING EXCEPT WINS AND LOSSES COUNTERS**
 			//increment loss counter 
 			//guess counter resets
 		
