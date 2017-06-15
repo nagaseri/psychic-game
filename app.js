61677f9b6c74fc8029ceeca9b68aa2a635d01173
Eri <eriinwanderland@calvisitor-10-105-149-28.calvisitor.1918.berkeley.edu>
2017-06-15 09:59:47 -0700

    final push

--- app.js
+++ app.js
@@ -9,68 +9,60 @@
 
 //the computer picks a random letter
 	function computerMakesChoice() {
-		var alphabet = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
+		var alphabet = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
 		var randomIndex = Math.floor(Math.random () * alphabet.length);
-		console.log('randomIndex', randomIndex);
 //the var computer choice is the alphabet randomly chosen
-		var computerChoice = alphabet[randomIndex];
+		computerChoice = alphabet[randomIndex];
 		console.log('computerChoice', computerChoice);
 	}
-//calling the function for computer to make choice
-	computerMakesChoice();
-	
-//function to reset game	
+
+// function to reset game	
 	function gameReset(){
 		guessesLeft = 3;
 		wrongGuesses = [];
 		computerMakesChoice();
-
 	}
 
 //the user picks a letter of the alphabet 
-		document.onkeyup = function(event){
+		document.onkeyup = function(event) {
+			//the key the user presses becomes userChoice
 			userChoice = event.key;
+			// var userGuess = String.fromCharCode(event.keyCode).toLowerCase();
+			console.log(userChoice);
 			//if user guesses right letter, WIN
 			//increment wins counter
 			//reset guesses left, wrong guesses, and randomize computer choice
 			if(userChoice === computerChoice){
 				wins++;
-				gameReset();
+				alert("Awesome, you are psychic!");
+				document.querySelector('#wins').innerHTML = "Wins: " + wins;
 				console.log('wins', wins);
+				console.log(computerChoice);
+				gameReset();
+			}	
+			else if(guessesLeft === 0){
+				losses++;
+				document.querySelector('#losses').innerHTML = "Losses: " + losses;
+				alert("You are not psychic! That's okay.");
+				console.log('losses', losses);
+				console.log('guessesLeft', guessesLeft);
+				console.log(computerChoice);
+				gameReset();
 			}
-			//if not, guesses left decreases
-			//push current user choice to array of wrongguesses
+			// if not, guesses left decreases
+			// push current user choice to array of wrongguesses
 			else {
 				guessesLeft--;
 				wrongGuesses.push(userChoice);
+				document.querySelector('#guessesLeft').innerHTML = "Guesses left: " + guessesLeft;
+				document.querySelector('#wrongGuesses').innerHTML = "Letters used: " + wrongGuesses;
 				console.log('wrongGuesses', wrongGuesses);
 				console.log('userChoice', userChoice);
 				console.log('guessesLeft', guessesLeft);
-			}
-			//if there are no guesses left
-			//increment loss counter
-			//reset game
-			if(guessesLeft === 0){
-				losses++;
-				gameReset();
-				console.log('losses', losses);
-				console.log('guessesLeft', guessesLeft);
-			}
-			// if(userChoice === alphabet){
-			// 	userChoice.push(wrongGuesses);
-			// 	alert("You already guessed this letter, try again!")
-			// }
-			}
+				console.log(computerChoice);
+			}}
 
+			gameReset();
+		
 
-//if they don't match
-	//decrement guess counter
-	//if user has guesses left
-		//they get to guess again
-		//push the wrong letter guessed into array of wrong guesses
-	//if user has 0 guesses left
-		//user loses
-		//restart the game **RESET EVERTYTHING EXCEPT WINS AND LOSSES COUNTERS**
-			//increment loss counter 
-			//guess counter resets
 		
